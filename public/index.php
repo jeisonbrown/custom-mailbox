b744c60787c2ad387969f24622032df275899287
Santiago Ruiz <santiago.ruiz@bewe.io>
2020-04-16 08:35:19 -0500

    base project

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php 
+
+define('DEBUG', true);
+
+require __DIR__.'/../bootstrap/autoload.php';
+
+require __DIR__.'/../bootstrap/views.php';
+
+// $template = $twig->load('prueba.html');
+// var_dump($template) ;
+//echo $twig->render('index', ['name' => 'Fabien']);
